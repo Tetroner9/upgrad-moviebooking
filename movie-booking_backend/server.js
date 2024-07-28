67ae3bfb8e5683b2c95d3d889c8d4da86ff3d736
Tetroner9 <omkadam2125@gmail.com>
2024-07-28 22:19:42 +0530

    minor changes

--- movie-booking_backend/server.js
+++ movie-booking_backend/server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 });
 
 // Set the port and start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8085;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
